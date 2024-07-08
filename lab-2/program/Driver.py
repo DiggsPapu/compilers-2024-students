ece1d0ab55d44d06acd2d278ee3e5f88b28c160a
DiggsPapu <diggs.papu@gmail.com>
2024-07-08 16:32:00 -0600

    update:ShowTree

--- lab-2/program/Driver.py
+++ lab-2/program/Driver.py
@@ -4,11 +4,13 @@ from MiniLangLexer import MiniLangLexer
 from MiniLangParser import MiniLangParser
 
 def main(argv):
+    # String of input
     input_stream = FileStream(argv[1])
     lexer = MiniLangLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = MiniLangParser(stream)
     tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
+    print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
